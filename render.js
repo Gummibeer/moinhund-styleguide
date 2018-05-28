22b42e717f4b78fab364e9e14c3b6fb32eac0bcd
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-28 14:31:46 +0200

    add spacing to image

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 var page = require('webpage').create();
-page.viewportSize = { width: 1280, height: 768 };
+page.viewportSize = { width: 1440, height: 768 };
 page.open('index.html', function() {
     page.render('styleguide.png');
     phantom.exit();
